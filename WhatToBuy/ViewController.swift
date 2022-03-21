c295f0cf551ff3d1fb19736202189abc1e87f55b
Oleg Stepanov <olezhka106s@gmail.com>
2022-03-21 19:36:37 +0300

    Initial Commit

---
+++ WhatToBuy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatToBuy
+//
+//  Created by Oleg Stepanov on 21.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
